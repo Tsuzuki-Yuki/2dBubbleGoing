f9caf4464e18cbb3f3055c3e4255736b082d22c8
Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-06-02 00:05:57 +0900

    Create project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	enum State {
+		Ready,
+		Play,
+		GameOver
+	}
+
+	State state;
+
+	public BubbleController bubble;
+	public GameObject blocks;
+
+	// Use this for initialization
+	void Start () {
+		Ready ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ゲームステートメントごとにイベントを取得
+		switch (state) {
+		case State.Ready:
+			//タッチしたらゲームスタート
+			if (Input.GetButtonDown ("Fire1")) {
+				GameStart ();
+			}
+			break;
+		case State.Play:
+			//バブルが死んだらゲームオーバー
+			if (bubble.IsDead ()) {
+				GameOver ();
+			}
+			break;
+		case State.GameOver:
+			//タッチしたらシーンのリロード
+			if (Input.GetButtonDown ("Fire1")) {
+				Reload ();
+			}
+			break;
+		}
+	}
+
+	void Ready(){
+		state = State.Ready;
+		blocks.SetActive (false);
+	}
+
+	void GameStart(){
+		state = State.Play;
+		blocks.SetActive (true);
+	}
+
+	void GameOver(){
+		state = State.GameOver;
+		//シーン中の全てのScrollObjectコンポーネントを探し出す
+		ScrollObjects[] scrollObjects = GameObject.FindObjectsOfType<ScrollObjects> ();
+		//全ScrollObjectのスクロール処理を無効にする
+		foreach (ScrollObjects scroll in scrollObjects) {
+			scroll.enabled = false;
+		}
+	}
+
+	void Reload(){
+		//Mainシーンを再読み込み
+		SceneManager.LoadScene ("Main");
+	}
+}
---
+++ Assets/Scripts/ScrollObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObjects : MonoBehaviour {
+
+	public float speed;
+	public float startPosition;
+	public float endPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-1 * speed * Time.deltaTime, 0, 0);
+
+		//スクロールが目標ポイントまで到達したかのチェック
+		if (transform.position.x <= endPosition) {
+			ScrollEnd ();
+		}
+	}
+
+	//スクロールする距離分を戻す
+	void ScrollEnd(){
+		transform.Translate (-1 * (endPosition - startPosition), 0, 0);
+	}
+}
